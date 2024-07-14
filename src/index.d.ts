ff803e1aefbfea588d9f07929e9925ee1f268ce7
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-07-14 21:18:20 +0300

    initial commit

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,16 @@
-export * from "./source/sound";
-export * from "./source/soundManager";
+interface MelodySound extends Sound {
+	Priority: number;
+	readonly MuteOthers: boolean;
+	Play(muteOthers?: boolean): void;
+}
+
+interface SoundManager {
+	create: <T extends Record<string, string>>(obj: T) => { [P in keyof T]: () => MelodySound };
+}
+
+interface SoundCtor {
+	new (instance: Instance): MelodySound;
+}
+
+export declare const Manager: SoundManager;
+export declare const Sound: SoundCtor;
